fdf73b3ee5417b71bb452dddd21cd9803301ab78
Marius Birsan <marius.birsan90@outlook.com>
2023-11-26 17:48:51 +0200

    Files were updated

---
+++ src/js/api.js
@@ -0,0 +1,13 @@
+export const BASE_URL = 'https://pixabay.com/api/';
+export const API_KEY = '40903961-e07635d5dbea58b7d17abeecb';
+export const options = {
+  params: {
+    key: API_KEY,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 40,
+    page: 1,
+    q: '',
+  },
+};
